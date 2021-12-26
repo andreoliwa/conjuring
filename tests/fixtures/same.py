29fdcdb42f45df4121bafbc0dfbbb4406b0edda1
W. Augusto Andreoli <andreoliwa@gmail.com>
2021-12-26 12:47:51 +0100

    test: task with same name (but it's failing)

---
+++ tests/fixtures/same.py
@@ -0,0 +1,6 @@
+from invoke import task
+
+
+@task
+def task_c(c):
+    pass
--- tests/test_conjuring.py
+++ tests/test_conjuring.py
@@ -12,25 +12,29 @@ def my_collection():
     return Collection("mine")
 
 
-def assert_tasks(collection: Collection, tasks: List[str]):
-    assert list(collection.task_names.keys()) == tasks
+def assert_tasks(collection: Collection, task_names: List[str], *, by_name=False):
+    # TODO: Something weird in tests: the "collection.tasks" is empty in some cases
+    if by_name:
+        assert list(collection.task_names.keys()) == task_names
+    else:
+        assert list(collection.tasks.keys()) == task_names
 
 
 def test_prefixed_tasks(my_collection):
     from tests.fixtures import prefixed
 
     magically_add_tasks(my_collection, prefixed)
-    assert_tasks(my_collection, ["prefixed.task-a", "prefixed.task-b"])
+    assert_tasks(my_collection, ["prefixed.task-a", "prefixed.task-b"], by_name=True)
 
 
 def test_add_module_by_name(my_collection):
     magically_add_tasks(my_collection, "tests.fixtures.prefixed")
-    assert_tasks(my_collection, ["prefixed.task-a", "prefixed.task-b"])
+    assert_tasks(my_collection, ["prefixed.task-a", "prefixed.task-b"], by_name=True)
 
 
 def test_create_collection_from_glob_python_patterns():
     coll = collection_from_python_files("tests.fixtures.root", "glob*.py")
-    assert_tasks(coll, ["glob-c", "glob-d", "root.a", "root.b"])
+    assert_tasks(coll, ["glob-c", "glob-d", "root.a", "root.b"], by_name=True)
 
 
 def test_not_prefixed_tasks(my_collection):
@@ -70,8 +74,13 @@ def test_module_with_magic_task(my_collection):
     assert_tasks(my_collection, ["depends-on-the-module-config", "this-task-is-always-visible"])
 
 
-def test_add_task_with_the_same_name():
-    pass  # TODO:
+@pytest.mark.xfail(reason="Empty collection.tasks is causing this test to fail")
+def test_add_task_with_the_same_name(my_collection):
+    from tests.fixtures import not_prefixed, same
+
+    magically_add_tasks(my_collection, not_prefixed)
+    magically_add_tasks(my_collection, same)
+    assert_tasks(my_collection, ["task-c", "task-d", "task-d-same"])
 
 
 def test_add_sub_collection_with_same_name_as_task():
