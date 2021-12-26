b4d6128f0245571210d6bcdd842278bbe94b675d
W. Augusto Andreoli <andreoliwa@gmail.com>
2021-12-26 13:19:24 +0100

    feat(poetry): choose pipx repo to inject with fzf

--- src/conjuring/spells/poetry.py
+++ src/conjuring/spells/poetry.py
@@ -1,24 +1,35 @@
 from invoke import task
 
+from conjuring.grimoire import run_command, print_error, run_with_fzf
 from conjuring.visibility import ShouldDisplayTasks, has_pyproject_toml
 
 SHOULD_PREFIX = True
 should_display_tasks: ShouldDisplayTasks = has_pyproject_toml
 
 
-@task
-def editable(c, inject=True):
+@task(help={"inject": "Pipx repo to inject this project into"})
+def editable(c, inject=""):
     """Hack to install a Poetry package as editable until Poetry supports PEP660 hooks.
 
-    It won't be needed when https://github.com/python-poetry/poetry-core/pull/182 is merged.
+    It won't be needed anymore when https://github.com/python-poetry/poetry-core/pull/182 is merged.
     """
+    chosen_repo = ""
+    if inject:
+        # Ask for the repo before doing anything else... to fail fast if no repo is chosen
+        chosen_repo = run_with_fzf(c, "ls -1 ~/.local/pipx/venvs/", query=inject)
+        if not chosen_repo:
+            return
+
     c.run("poetry build")
     c.run("tar -xvzf dist/*.gz --strip-components 1 */setup.py")
     # Ignore errors, it might not be installed
     c.run("black setup.py", warn=True)
 
-    if not inject:
+    if not chosen_repo:
+        print_error("Use --inject to inject this repo into a pipx virtualenv.")
         return
+
     c.run("mv pyproject.toml _pyproject.toml")
-    c.run("pipx inject -e invoke .")
+    run_command(c, "pipx inject -e", chosen_repo, ".")
     c.run("mv _pyproject.toml pyproject.toml")
+    c.run("rm setup.py")
