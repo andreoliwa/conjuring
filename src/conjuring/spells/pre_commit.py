e1b07279b2bfbcdadec57cb4c24aa788aa503289
W. Augusto Andreoli <andreoliwa@gmail.com>
2021-12-28 14:13:53 +0100

    feat(pre-commit): command to uninstall hooks

--- src/conjuring/spells/pre_commit.py
+++ src/conjuring/spells/pre_commit.py
@@ -8,13 +8,27 @@ from conjuring.visibility import ShouldDisplayTasks, has_pre_commit_config_yaml
 SHOULD_PREFIX = True
 should_display_tasks: ShouldDisplayTasks = has_pre_commit_config_yaml
 
+GIT_HOOKS = "-t pre-commit -t commit-msg"
 
-@task
+
+def _run_garbage_collector(c):
+    c.run("pre-commit gc")
+
+
+@task(help={"gc": "Run the garbage collector to remove unused venvs"})
 def install(c, gc=False):
     """Pre-commit install scripts and hooks."""
     if gc:
-        c.run("pre-commit gc")
-    c.run("pre-commit install -t pre-commit -t commit-msg --install-hooks")
+        _run_garbage_collector(c)
+    c.run(f"pre-commit install {GIT_HOOKS} --install-hooks")
+
+
+@task(help={"gc": "Run the garbage collector to remove unused venvs"})
+def uninstall(c, gc=False):
+    """Pre-commit uninstall scripts and hooks."""
+    if gc:
+        _run_garbage_collector(c)
+    c.run(f"pre-commit uninstall {GIT_HOOKS}")
 
 
 @task
