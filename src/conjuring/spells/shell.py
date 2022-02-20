516cc1734e633a2778ee4f89b0c0625599f33cb1
W. Augusto Andreoli <andreoliwa@gmail.com>
2022-02-20 17:40:24 +0100

    feat(shell): list and uninstall shell completions

--- src/conjuring/spells/shell.py
+++ src/conjuring/spells/shell.py
@@ -3,20 +3,37 @@ from invoke import task
 
 SHOULD_PREFIX = True
 
+COMPAT_DIR = "$BASH_COMPLETION_COMPAT_DIR/"
+USER_DIR = "$BASH_COMPLETION_USER_DIR/completions/"
+COMPLETION_DIRS = (COMPAT_DIR, USER_DIR)
+
 
 @task
-def click_completion(c, app, uninstall=False):
-    """Click completion for Bash.
+def completion_list(c):
+    """List existing shell completions."""
+    for var in COMPLETION_DIRS:
+        c.run(f"exa -l {var}")
+
+
+@task
+def completion_install(c, app):
+    """Install shell completion. For now, only for the Bash shell, and only for Click projects.
 
     See:
     - https://click.palletsprojects.com/en/8.0.x/shell-completion/
     - https://github.com/click-contrib/click-completion
     """
-    completion_file = f"$BASH_COMPLETION_COMPAT_DIR/{app}.bash-completion"
-    if uninstall:
-        c.run(f"rm {completion_file}", warn=True)
-        c.run("exa -l $BASH_COMPLETION_COMPAT_DIR/")
-        return
-
+    completion_file = f"{COMPAT_DIR}{app}.bash-completion"
     c.run(f"_{app.upper()}_COMPLETE=bash_source {app} > {completion_file}")
     c.run(f"exa -l {completion_file}")
+    c.run(f"bat {completion_file}")
+
+
+@task
+def completion_uninstall(c, app):
+    """Uninstall shell completion from both completion dirs."""
+    for completion_dir in COMPLETION_DIRS:
+        with c.cd(completion_dir):
+            c.run(f"rm -v {app}*", warn=True)
+    completion_list(c)
+    return
